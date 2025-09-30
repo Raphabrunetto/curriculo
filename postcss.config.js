27123e55ed46c7bc96724b00c120c35c8b80f9c0
Raphael Augusto <133877084+Raphabrunetto@users.noreply.github.com>
2025-09-30 12:51:22 -0300

    fix(vercel): Final PostCSS simplification to load basic Tailwind setup for successful Vercel deploy

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    // Retornamos à sintaxe simples, confiando que o Next.js fará o restante
+    // Isso é a sintaxe mais básica e menos propensa a erros na Vercel
     tailwindcss: {}, 
     autoprefixer: {},
   },
