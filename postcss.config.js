ef9e7767d40eb4a3d57b207596ad28717940b8da
Raphael Augusto <133877084+Raphabrunetto@users.noreply.github.com>
2025-09-30 12:42:38 -0300

    fix(vercel): Reverting postcss.config.js to module.exports to fix Vercel build failure

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
-// Remova qualquer 'import' ou 'export default' e use a sintaxe abaixo:
-
 module.exports = {
-  // O Next.js exige que a chave principal seja 'plugins'
   plugins: {
-    // Usamos o pacote correto do Tailwind (instalado anteriormente)
+    // Usamos o pacote correto do Tailwind
     '@tailwindcss/postcss': {}, 
     autoprefixer: {},
   },
