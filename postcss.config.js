4f40122fbc698763ed519ec482947f08981d6179
Raphael Augusto <133877084+Raphabrunetto@users.noreply.github.com>
2025-09-30 12:47:04 -0300

    fix(vercel): Reverted PostCSS to standard tailwindcss: {} to fix build path error on Vercel

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: {
-    // Usamos o pacote correto do Tailwind
-    '@tailwindcss/postcss': {}, 
+    // Retornamos à sintaxe simples, confiando que o Next.js fará o restante
+    tailwindcss: {}, 
     autoprefixer: {},
   },
 };
